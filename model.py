140c766a0a4ed46745a675b69224bde0e72bd747
Erik01010 <erik.reitsam@hotmail.de>
2025-10-03 23:54:40 +0200

    added one_hot_encoding, use DecisionTreeClassifier.

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from feature_engineering import process_data
 from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 
 def calc_success_rate(df: pd.DataFrame) -> float:
@@ -11,10 +12,26 @@ def calc_avg_transaction_costs(df: pd.DataFrame) -> float:
     return round(df["cost"].mean(), 2)
 
 
+data = process_data()
+y = data["success", "PSP"]
+X = data.drop(columns=["success", "PSP"])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+decision_tree_model = tree.DecisionTreeClassifier(random_state=42)
+decision_tree_model.fit(X_train, y_train)
+preds = decision_tree_model.predict(X_test)
+
+
 class BaseLineModel:
     """Class for Baseline model."""
 
-
+    def __init__(self):
+        self.model = tree.DecisionTreeClassifier(random_state=42)
+        self.model.fit(X_train, y_train)
+        self.model.predict(X_test)
 
 
 if __name__ == "__main__":
